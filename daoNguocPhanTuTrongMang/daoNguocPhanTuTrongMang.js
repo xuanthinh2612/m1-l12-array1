980797a85715626e7bab4fcf10be0c67b59cf425
gio lang du <you@example.com>
2020-11-18 11:36:11 +0700

    reverse array

---
+++ daoNguocPhanTuTrongMang/daoNguocPhanTuTrongMang.js
@@ -0,0 +1,11 @@
+let arr1 = [-3, 5, 1, 3, 2, 10]
+document.write('arr1: ' + arr1 + '<br>');
+
+function reserves() {
+    let arr2 = [];
+    for (let i = 0; i < arr1.length; i++) {
+        let a = arr1[arr1.length - (i + 1)];
+        arr2.push(a);
+    }
+    console.log(arr2);
+}
